92a8795cddc6cbe5c93d1752527d3946d0e9f9fb
christinamcmahon <christina.mcmahon1@gmail.com>
2020-01-15 16:09:38 -0800

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,16 +6,9 @@ class Song < ApplicationRecord
   validate :no_repeats
   validates :released, inclusion: { in: [true, false] }
   validate :not_in_future
+  validates :title, uniqueness: { scope: [:artist_name, :release_year], message: "Cannot add the same song twice in one year." }
 
   # custom validations
-  def no_repeats
-    Song.all.each do |song|
-      if song.title == title && song.artist_name == artist_name && song.release_year == release_year
-        errors[:title] << "Cannot add the same song twice in one year."
-      end
-    end
-  end
-
   def not_in_future
     if release_year.present? && release_year > Date.today.year
       errors[:release_year] << "Release year cannot be in the future."
